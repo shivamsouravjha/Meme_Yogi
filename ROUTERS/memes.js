17fe989a520450f2a5b36efce170943f8d0a67f0
KaanGaming <kaangaming.gamejolt@gmail.com>
2021-02-12 20:18:42 +0300

    Propose a lot of fix for #1

--- ROUTERS/memes.js
+++ ROUTERS/memes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { check } = require('express-validator');
 const router = express.Router();
 const memescontroller = require('../CONTROLLERS/memescontroller');
+const { fileUpload } = require('../middleware/file-upload');
+
 router.get('/:memeid', memescontroller.MemesbyID);
 router.get('/memer/:memerid', memescontroller.Memesbymemer);
 router.get('/', memescontroller.Getallmemes);
