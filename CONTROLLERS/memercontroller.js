a2ac20bdf8dcbcb5ae1b46cb351a3d225ce4d690
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-06-05 16:28:34 +0530

    deploying login and signup

--- CONTROLLERS/memercontroller.js
+++ CONTROLLERS/memercontroller.js
@@ -1,6 +1,5 @@
 const fs=require('fs');
 var path = require('path');
-
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const MemerSchema = require('../models/memer-schema');
